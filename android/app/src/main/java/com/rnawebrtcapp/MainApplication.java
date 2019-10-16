e8d8fcf32ef06aff48a4fe942fce94d5c37542fa
Ayan Banerjee <ayanb1999@gmail.com>
2019-10-17 01:39:04 +0530

    sending geolocation data to server

--- android/app/src/main/java/com/rnawebrtcapp/MainApplication.java
+++ android/app/src/main/java/com/rnawebrtcapp/MainApplication.java
@@ -7,11 +7,12 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactnativecommunity.geolocation.GeolocationPackage;
+import com.github.wumke.RNExitApp.RNExitAppPackage;
+
 
 import java.util.List;
 import com.oney.WebRTCModule.WebRTCModulePackage;
